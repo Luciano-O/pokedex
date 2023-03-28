d6e93f5f1917bc3d5b9010db3c8b9555f3e8161f
Luciano <oliveiraluck78@gmail.com>
2023-03-28 04:01:16 -0300

    input styles complete

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,25 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import pokeball from '../Assets/Pokeball.png'
 import styles from '../styles/Header.module.scss'
 
 export default function Header() {
   return(
-    <>
+    <header className={styles.header}>
       <Image src={pokeball} alt='pokebola' className={styles.pokeball} />
-    </>
+      <div className={styles.input_container}>
+        <FontAwesomeIcon 
+          icon={faMagnifyingGlass} 
+          style={{color: "#666666",}}
+          className={styles.search_icon}
+        />  
+        <input 
+          type='text'
+          placeholder='Insira o nome ou numero'
+          className={styles.search_input}
+        />
+      </div>
+    </header>
   )
 }
