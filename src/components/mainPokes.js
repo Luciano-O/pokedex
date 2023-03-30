898181b95942b4432e2cf4827855e02fa82080f0
Luciano <oliveiraluck78@gmail.com>
2023-03-30 03:49:08 -0300

    Load more pokemons implemented

--- src/components/mainPokes.js
+++ src/components/mainPokes.js
@@ -6,6 +6,8 @@ import PokeCard from "./pokeCard"
 
 export default function MainPokes() {
   const [pokes, setPokes] = useState([])
+  const [limit, setLimit] = useState(24)
+  const [allResults, setAllResults] = useState([])
   const { allPokes, search } = useContext(Poke_data)
   const [loading, setLoading] = useState(true)
 
@@ -13,23 +15,39 @@ export default function MainPokes() {
     const bringPokes = async () => {
       const filtred = allPokes.filter(({name}) => name.includes(search))
 
-      console.log(filtred);
-
       const results = filtred.slice(0, 12)
+
+      setLimit(24)
 
       setPokes(results)
 
       setLoading(false)
+
+      setAllResults(filtred)
     }
 
     bringPokes()
   }, [allPokes, search])
+
+  const handleButton = () => {
+    setLimit(limit + 12)
+
+    const newFilter = allResults.slice(limit - 12, limit)
+
+    setPokes([...pokes, ...newFilter])
+  }
 
   return(
     <main className={styles.main_pokes}>
       {loading ? 
         'Carregando' : 
         pokes.map((poke) => <PokeCard key={poke.name} url={poke.url} />)}
+      <button
+        type="button"
+        onClick={handleButton}
+      >
+        Carregar mais
+      </button>
     </main>
   )
 }
