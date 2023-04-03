ac971f3378896019daba83042767b9a152db609f
Luciano <oliveiraluck78@gmail.com>
2023-04-03 15:28:35 -0300

    Tab fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import Header from "@/components/header";
 import MainPokes from "@/components/mainPokes";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Pokedex</title>
+      </Head>
       <Header />
       <MainPokes />
     </>
